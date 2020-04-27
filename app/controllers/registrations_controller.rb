8cb4d78d6f2c7954af89fc156365945bbb9d4068
trox115 <tofernandes67@gmail.com>
2020-04-27 19:55:10 +0100

    made session create controller and registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController <ApplicationControoler
+  def create
+    user= User.create!(
+      email: params["user"]["email"],
+      password: params["user"]["password"],
+      password_confirmation: params["user"]["password_confirmation"],
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created
+      }
+    else
+      render json: {
+        status: 500
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :registrations, only:[:create]
+  resources :sessions, only:[:create]
   root to: "static#home"
 end
