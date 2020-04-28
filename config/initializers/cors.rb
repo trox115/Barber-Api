db0d3fddd55ffacfe9d9b780cdad79ba151a4bf7
trox115 <tofernandes67@gmail.com>
2020-04-28 18:32:18 +0100

    removing file

--- config/initializers/cors.rb
+++
@@ -1,6 +0,0 @@
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options], :credentials => true 
-  end 
-end
