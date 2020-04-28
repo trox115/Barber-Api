0ee189553bd45689b2e175ebd5fc11245ba0bc1b
trox115 <tofernandes67@gmail.com>
2020-04-28 21:44:59 +0100

    still figuring out cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'https://antoniobarberapi.herokuapp.com/'
+    origins 'localhost:3000'
     resource '*',
       :headers => :any,
       :methods => [:get, :post, :delete, :put, :patch, :options, :head],
