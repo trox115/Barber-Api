754de183ed4864cf1b53400b60aece647c661757
trox115 <tofernandes67@gmail.com>
2020-05-05 08:37:37 +0100

    added new store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,2 @@
-if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain:"https://antoniobarberapi.herokuapp.com"
-else
 Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
   
