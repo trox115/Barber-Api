d677a5756dba728ce73d0d51e24ba61d8da52b2e
trox115 <tofernandes67@gmail.com>
2020-05-05 06:26:53 +0100

    cors does not work on heroku

--- app/controllers/barbers_controller.rb
+++ app/controllers/barbers_controller.rb
@@ -1,5 +1,5 @@
 class BarbersController < ApplicationController
-
+    before_action :cors_set_access_control_headers
   # GET /barbers
   def index
     @barber=Barber.all
@@ -11,5 +11,14 @@ class BarbersController < ApplicationController
   def show
     render json: @barber
   end
+def cors_set_access_control_headers
+  response.headers["Cache-Control"]="max-age=3600"
+  response.headers['Access-Control-Allow-Origin'] = 'https://antoniobarberapi.herokuapp.com/home'
+  response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, ' \
+    'Auth-Token, Email, X-User-Token, X-User-Email, x-xsrf-token'
+  response.headers['Access-Control-Max-Age'] = '1728000'
+  response.headers['Access-Control-Allow-Credentials'] = true
+end
 end
 
