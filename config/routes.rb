f88b7180e9aea072a47fe692354c931415acac71
trox115 <tofernandes67@gmail.com>
2020-05-06 23:18:52 +0100

    deleted duplicated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
-
   resources :registrations, only:[:create]
-  resources :bookings, only:[:create ,:get]
-  resources :sessions, only:[:create ,:get]
   resources :barbers, only:[:show,:get]
   delete :logout, to: "sessions#logout"
-  delete :logout, to: "sessions#logout"
-  get :sessions, to: "sessions#create"
+  post :sessions, to: "sessions#create"
   get :loggedin, to: "sessions#logged_in"
-  get :barbers, to: "barbers#index"
   get :bookings, to: "bookings#index"
   get :show, to: "bookings#show"
   post :bookings, to: "bookings#create"
