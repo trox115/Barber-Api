e87c86bbd46fe2829e609bbe95ff234667a24680
trox115 <tofernandes67@gmail.com>
2020-05-07 04:54:25 +0100

    corrected routes to barbers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
   resources :registrations, only:[:create]
-  resources :barbers, only:[:show,:get]
   delete :logout, to: "sessions#logout"
   post :sessions, to: "sessions#create"
   get :loggedin, to: "sessions#logged_in"
   get :bookings, to: "bookings#index"
+  get :barbers , to: "barbers#index"
   get :show, to: "bookings#show"
   post :bookings, to: "bookings#create"
   root to: "static#home"
